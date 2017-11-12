b127609a29a5f2209a4d3fc2f6d8028fb323c4b0
javiergf14 <antoniojavier.gonzalez.ferrer@alumnos.upm.es>
2017-11-12 12:25:29 +0100

    Refactor triest algorithms, using abstract class and polymorphism for different behaviors.

---
+++ triest/algorithms.py
@@ -0,0 +1,80 @@
+from abc import ABC, abstractmethod
+
+import collections, random
+from triest.graph import EdgeSample
+from triest.stream_graph import EdgeStreamElement, Symbol
+
+
+class AbstractTriest(ABC):
+
+    def __init__(self, edge_stream, M):
+        self.edge_stream = edge_stream
+        self.M = M
+        self.S = EdgeSample()
+        self.t = 0
+        self.tau = 0
+        self.local_tau = collections.defaultdict(int)
+
+    @abstractmethod
+    def run(self):
+        for edge_stream_element in self.edge_stream:
+            self.t += 1
+            if isinstance(self, TriestImpr): self.update_counters(edge_stream_element)
+
+            if self.sample_edge(edge_stream_element.edge, self.t):
+                self.S.append(edge_stream_element.edge)
+                if isinstance(self, TriestBase): self.update_counters(edge_stream_element)
+
+    def sample_edge(self, edge, t):
+        if t <= self.M:
+            return True
+        elif self.flip_biased_coin(self.M/t):
+            random_int = random.randint(0, len(self.S)-1)
+            random_edge = self.S.pop(random_int)
+
+            if isinstance(self, TriestBase):
+                new_stream_element = EdgeStreamElement(Symbol.MINUS, random_edge)
+                self.update_counters(new_stream_element)
+            return True
+        return False
+
+    def update_counters(self, edge_stream_element):
+        shared_neighborhood = self.S.get_shared_neighborhood(edge_stream_element.edge)
+
+        if isinstance(self, TriestBase):
+            operator = 1 if edge_stream_element.symbol == Symbol.PLUS else -1
+        elif isinstance(self, TriestImpr):
+            operator = int(max(1, ((self.t - 1) * (self.t - 2)) / (self.M * (self.M - 1))))
+        else:
+            raise BaseException("Unsupported subclass algorithm: {}".format(self.__class__.__name__))
+
+        for c in shared_neighborhood:
+            self.tau += operator
+            self.local_tau[c] += operator
+            self.local_tau[edge_stream_element.edge.u] += operator
+            self.local_tau[edge_stream_element.edge.v] += operator
+
+    @staticmethod
+    def flip_biased_coin(head_prob):
+        return random.random() <= head_prob
+
+    def triangles_estimation(self):
+        if isinstance(self, TriestBase):
+            xi = max(1, (self.t * (self.t - 1) * (self.t - 2)) / (self.M * (self.M - 1) * (self.M - 2)))
+            return int(xi) * self.tau
+        elif isinstance(self, TriestImpr):
+            return self.tau
+        else:
+            raise BaseException("Unsupported subclass algorithm: {}".format(self.__class__.__name__))
+
+
+class TriestBase(AbstractTriest):
+
+    def run(self):
+        super().run()
+
+
+class TriestImpr(AbstractTriest):
+
+    def run(self):
+        super().run()
--- triest/script.py
+++ triest/script.py
@@ -1,58 +1,7 @@
-import random
-import collections
-
-from triest.stream_graph import EdgeStream, EdgeStreamElement, Symbol
-from triest.graph import Graph, EdgeSample
+from triest.stream_graph import EdgeStream
+from triest.graph import Graph
+from triest.algorithms import TriestBase, TriestImpr
 open_file_as_graph = Graph.open_file_as_graph
-
-
-class Triest:
-    def __init__(self, edge_stream, M):
-        self.edge_stream = edge_stream
-        self.M = M
-        self.S = EdgeSample()
-        self.t = 0
-        self.tau = 0
-        self.local_tau = collections.defaultdict(int)
-
-    def init(self):
-        for edge_stream_element in self.edge_stream:
-            self.t += 1
-            if self.sample_edge(edge_stream_element.edge, self.t):
-                self.S.append(edge_stream_element.edge)
-                self.update_counters(edge_stream_element)
-
-    def sample_edge(self, edge, t):
-        if t <= self.M:
-            return True
-        elif self.flip_biased_coin(self.M/t):
-            random_int = random.randint(0, len(self.S)-1)
-            random_edge = self.S.pop(random_int)
-
-            new_stream_element = EdgeStreamElement(Symbol.MINUS, random_edge)
-            self.update_counters(new_stream_element)
-            return True
-        return False
-
-    def update_counters(self, edge_stream_element):
-        shared_neighborhood = self.S.get_shared_neighborhood(edge_stream_element.edge)
-        operator = 1 if edge_stream_element.symbol == Symbol.PLUS else -1
-
-        for c in shared_neighborhood:
-            self.tau += operator
-            self.local_tau[c] += operator
-            self.local_tau[edge_stream_element.edge.u] += operator
-            self.local_tau[edge_stream_element.edge.v] += operator
-
-    @staticmethod
-    def flip_biased_coin(head_prob):
-        return random.random() <= head_prob
-
-    def triangles_estimation(self):
-        estimation = (self.t * (self.t - 1) * (self.t - 2)) / (self.M * (self.M - 1) * (self.M - 2))
-        xi = int(max(1, estimation))
-        return xi * self.tau
-
 
 if __name__ == "__main__":
 
@@ -60,13 +9,16 @@ if __name__ == "__main__":
     graph = open_file_as_graph(file)
     edge_stream = EdgeStream(graph.edges)
 
-    M = 3000
-    tr = Triest(edge_stream.elements, M)
+    M = 2500
+    tb = TriestBase(edge_stream.elements, M)
 
-    tr.init()
-    print(tr.triangles_estimation())
-    print(tr.t)
+    tb.run()
+    print(tb.t)
+    print(tb.triangles_estimation())
+
+    ti = TriestImpr(edge_stream.elements, M)
+    ti.run()
+    print(ti.triangles_estimation())
 
     # if t <= M, then no error
     # if t > M, there is variance within the error
-
